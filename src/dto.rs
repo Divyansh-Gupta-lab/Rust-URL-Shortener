9d37aa50f6d7334df28bb0c93fca49d874446fe1
Divyansh-Gupta-lab <divyansh181199@gmail.com>
2026-08-21 17:04:09 +0530

    Define dto module

---
+++ src/dto.rs
@@ -0,0 +1,15 @@
+pub mod shorten;
+pub use shorten::ShortenUrl;
+
+pub mod app_state;
+pub use app_state::AppState;
+
+pub mod register;
+pub use register::Register;
+
+pub mod login;
+pub use login::Login;
+
+
+pub mod claims;
+pub use claims::Claim;
